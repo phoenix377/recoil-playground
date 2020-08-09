9cc8c008f78d5ebcd419f4490aa4e1a3f74ef469
phoenix377 <phoenix.number377@gmail.com>
2020-08-09 09:12:54 +0800

    feat: add components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import { RecoilRoot } from 'recoil';
+import React from "react";
+import { RecoilRoot } from "recoil";
 
-import './App.css';
+import Details from "./components/Details/Details";
+import Sidebar from "./components/Sidebar/Sidebar";
+
+import "./App.css";
 
 function App() {
   return (
     <RecoilRoot>
-      <div className="App" />
+      <div className="App">
+        <div>
+          <Sidebar />
+        </div>
+        <div>
+          <Details />
+        </div>
+      </div>
     </RecoilRoot>
   );
 }
